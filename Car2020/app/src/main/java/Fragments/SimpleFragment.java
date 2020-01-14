71af7d829cb66d929438fb2d8bb0d89246693873
diadestiny <1247889154@qq.com>
2020-01-14 15:15:57 +0800

    2020.1.14 第一次填坑

---
+++ Car2020/app/src/main/java/Fragments/SimpleFragment.java
@@ -0,0 +1,20 @@
+package Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.guet.car2020.R;
+
+public class SimpleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag,container,false);
+        return view;
+    }
+}
